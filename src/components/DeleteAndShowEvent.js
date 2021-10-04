b91c6853d67eede9bca0a952e7dca7fa692ab3d7
OmarAlbarghouthi <omar.qb8077@gmail.com>
2021-10-04 18:19:38 +0300

    get from db func and delete func

--- src/components/DeleteAndShowEvent.js
+++ src/components/DeleteAndShowEvent.js
@@ -1,26 +1,22 @@
 import React, { Component } from 'react';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import axios from "axios";
 // import Row from "react-bootstrap/Row";
 
-class Event extends Component {
+class DeleteAndShowEvent extends Component {
 
-    // Event
-    addBookHandler = () => {
 
+    deleteEventHandler = () => {
         console.log(this.props.event)
-        this.props.addEventFunc(this.props.event)
+        this.props.deleteEventFunc(this.props.event._id)
     }
+
 
 
 
     render() {
         return (
             <div key={this.props.idx}>
-
-
                 <Card style={{ width: '18rem' }}>
                     {/* <Row xs={1} md={4} class="g-4"> */}
                         <Card.Img variant="top" src={this.props.event.image} />
@@ -34,7 +30,7 @@ class Event extends Component {
 
                             </Card.Text>
                             <Button variant='link' href={this.props.event.url}>Buy this ticket</Button>
-                            <Button onClick={this.addBookHandler}>Add To My Events</Button>
+                            <Button variant='danger' onClick={this.deleteEventHandler}>Delete</Button>
 
                         </Card.Body>
                     {/* </Row> */}
@@ -45,4 +41,4 @@ class Event extends Component {
     }
 }
 
-export default Event
+export default DeleteAndShowEvent
